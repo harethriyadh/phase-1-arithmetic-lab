47147c1efecb8a2b1a9ba6c6f29075be50818d0f
harith <hareth.cn12012@gmail.com>
2023-03-16 08:39:32 -0700

    Done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let multiply, num1, num2 ;
 
-// Write your code here
+num1 = 2 ;
+num2 = 31 ;
+
+multiply = num1* num2 ;
+
+multiply ;
+
+let random ;
+
+random = Math.ceil(Math.random()+1);
+
+random ;
+
+let mod, num3 = 8, num4 =  2 ;
+
+mod = num3 /  num4 ;
+
+mod ;
+
+let max = Math.max(11, 2, 3, 4, 20);
+
+max;
